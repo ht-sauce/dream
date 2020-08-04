ca8f3741091dc23b3893a42f2e4449ec67bf3f3e
daihaitian <daihaitian@126.com>
2020-08-04 12:07:41 +0800

    echarts组件优化

--- src/components/renderingEcharts/renderingEcharts.vue
+++ src/components/renderingEcharts/renderingEcharts.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="dht-rendering-echarts" :id="id"></div>
+  <div class="dht-rendering-echarts" ref="echarts"></div>
 </template>
 
 <script>
 import echarts from "echarts";
-import "echarts-gl";
+// import "echarts-gl";
 import chinaJson from "./china";
 export default {
   name: "dhtRenderingEcharts",
   props: {
-    id: String,
     chinajson: {
       type: Boolean,
       default: false
@@ -28,25 +27,33 @@ export default {
     }
   },
   beforeCreate() {},
-  created() {},
-  beforeMount() {},
   mounted() {
     this.$nextTick(() => {
-      //将数据返回出去标识加载完成
-      this.$emit("loadFinish", this.id);
       this.renderingEcharts();
+      this.onresize()
     });
-    //this.echarts.resize();
-    //IntersectionObserver尝试用这个来做监听
   },
   methods: {
+    // 监听页面大小变化
+    onresize() {
+      const resize = () => {
+        this.echarts && this.echarts.resize()
+      }
+      window.addEventListener('resize', resize)
+      this.$once('hook:beforeDestroy', () => {
+        window.removeEventListener('resize', resize)
+      })
+    },
     //渲染echarts
     renderingEcharts() {
       let dom = null;
       let timer = setInterval(() => {
-        dom = document.querySelector(`#${this.id}`);
+        dom = this.$refs.echarts
         this.chinajson && echarts.registerMap("china", chinaJson);
         if (dom && this.options) {
+          //将数据返回出去标识加载完成
+          this.$emit('loadFinish')
+
           let myChart = echarts.init(dom);
           window.onresize = myChart.resize;
           myChart.setOption(this.options);
